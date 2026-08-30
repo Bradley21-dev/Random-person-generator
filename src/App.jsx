9f5083303e65eedc767aa063d52f90a1d0df6641
Bradley21-dev <ellisseibeb20@gmail.com>
2026-08-30 08:32:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,10 @@ function App() {
 
   if (!user)
     return (
-      <div className="mt-10 p-5 text-cyan-950  flex flex-col gap-5 items-center animate-bounce text-5xl text-mono font-bold">
+      <div class="animate-spin h-10 w-10 border-4 rounded-full border-t-transparent border-blue-500"></div>
         {" "}
-        <div class="animate-spin h-10 w-10 border-4 rounded-full border-t-transparent border-blue-500"></div>
-      </div>
+        
+      
     );
   return (
     <div className="mt-10 mx-auto p-5 w-200 rounded-4xl flex flex-col text-white bg-emerald-900 gap-5 items-center">
